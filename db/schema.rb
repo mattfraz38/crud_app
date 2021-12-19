c84fa8846948298d3f5232f814715c11f5883544
Matt Frazier <frazier3388@yahoo.com>
2021-12-19 21:38:11 +0900

    Create model, controller, index and show views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_213136) do
+ActiveRecord::Schema.define(version: 2021_12_19_113853) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_12_15_213136) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
