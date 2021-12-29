0b55dc52fa721bc481bf39992f3633e08dc285dd
Matt Frazier <frazier3388@yahoo.com>
2021-12-29 21:51:09 +0900

    add js images folder, svg logo and favicon, change welcome styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ import "bootstrap"
 // import "bootstrap/dist/js/bootstrap"
 // import "bootstrap/dist/css/bootstrap"
 import "stylesheets/application"
+const images = require.context('../images', true)
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
