d9b1dfce7b5d4f2e822b7ab0dee27fe037456f3d
Matt Frazier <frazier3388@yahoo.com>
2022-01-17 22:24:36 +0900

    Add dynamic age to about controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,8 @@ class StaticPagesController < ApplicationController
   end
 
   def about
+    @now = Time.now
+    @birthday = Time.new(1988, 3, 3)
+    @age = @now.year - @birthday.year - ((@now.month > @birthday.month || (@now.month == @birthday.month && @now.day >= @birthday.day)) ? 0 : 1)
   end
 end
